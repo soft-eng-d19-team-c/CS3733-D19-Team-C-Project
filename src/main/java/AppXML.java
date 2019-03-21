c25855e9f9b9e59e8f521806c4d4b26b994a3e56
rmlamarche <rmlamarche@wpi.edu>
2019-03-21 10:51:30 -0400

    renamed views package to controller

--- src/main/java/AppXML.java
+++ src/main/java/AppXML.java
@@ -18,7 +18,7 @@ public class AppXML extends Application {
     @Override
     public void start(Stage s) {
         try {
-            Scene scene = new Scene(FXMLLoader.load(getClass().getResource("views/app.fxml")));
+            Scene scene = new Scene(FXMLLoader.load(getClass().getResource("controller/app.fxml")));
             s.setScene(scene);
             s.getIcons().add(new Image("img/icon.png"));
             s.setMaximized(true);
--- src/main/java/controller/App.java
+++ src/main/java/controller/App.java
@@ -1,4 +1,4 @@
-package views;
+package controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
