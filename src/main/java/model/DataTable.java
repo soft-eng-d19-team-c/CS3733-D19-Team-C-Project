70197a254a526b01c2110472c27fb0aca82f2045
Jacob Tutlis <jtutlis@gmail.com>
2019-03-24 18:15:02 -0400

    added getDataByID and getAllData, sql commands may not work

--- src/main/java/model/DataTable.java
+++ src/main/java/model/DataTable.java
@@ -11,19 +11,23 @@ public class DataTable {
 
     public DataTable() {
         this.data = FXCollections.observableArrayList();
-        this.data = FXCollections.observableArrayList(
-                new Node("id1", 1, 2),
-                new Node("New ID", 1242, 21)
-        );
+//        this.data = FXCollections.observableArrayList(
+//                new Node("id1", 1, 2),
+//                new Node("New ID", 1242, 21)
+//        );
         try {
             this.connection = DriverManager.getConnection("jdbc:derby:TeamC;create=true");
             System.out.println("Connected");
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+
+        System.out.println(getDataById("BHALL00802"));
     }
 
     public ObservableList<Node> getAllData() {
+
 //        //Node temp = new Node();
         try {
             Statement stmt = connection.createStatement();
@@ -31,12 +35,10 @@ public class DataTable {
             ResultSet rs = stmt.executeQuery(str);
 
             while(rs.next()) {
-                String ID = rs.getString("NodeID");
-                int x = rs.getInt("xcoord");
-                int y = rs.getInt("ycoord");
+                Node temp = parseResultSet(rs);
+                //System.out.println(ID);
 
-
-//                this.data.add();
+                this.data.add(temp);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -45,20 +47,55 @@ public class DataTable {
         return data;
     }
 
-    public boolean setData(Node n) {
-//        try {
-//            Statement stmt = connection.createStatement();
-//            String str = "UPDATE PROTOTYPENODES SET field=filedData WHERE ID="+ data[0];
-//            boolean rs = stmt.execute(str);
-//            return rs;
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+    public boolean setData(Node node) {
+
+
+        try {
+            Statement stmt = connection.createStatement();
+            String str = "UPDATE PROTOTYPENODES SET field=XCOORD WHERE ID=" + node.getID();
+            return stmt.execute(str);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
     public Node getDataById(String ID) {
+
+        try {
+
+            Statement stmt = connection.createStatement();
+            String str = "SELECT * FROM PROTOTYPENODES WHERE NODEID=" + ID;
+            ResultSet rs = stmt.executeQuery(str);
+            return parseResultSet(rs);
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return null;
+    }
+
+    private Node parseResultSet(ResultSet rs){
+
+        try {
+            String ID = rs.getString("NodeID");
+            int x = rs.getInt("xcoord");
+            int y = rs.getInt("ycoord");
+            String floor = rs.getString("floor");
+            String building = rs.getString("building");
+            String nodeType = rs.getString("nodetype");
+            String longName = rs.getString("longname");
+            String shortName = rs.getString("shortname");
+            return new Node(ID, x, y, floor, building, nodeType, longName, shortName);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+
+
     }
 
 }
