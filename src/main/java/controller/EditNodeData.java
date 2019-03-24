6ba6b6b7823fc8f2126a3399a387aafa3b602faf
Ryan LaMarche <rmlamarche@wpi.edu>
2019-03-24 17:04:53 -0400

    this is broken but i need to push :(

--- src/main/java/controller/EditNodeData.java
+++ src/main/java/controller/EditNodeData.java
@@ -1,13 +1,30 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import model.Node;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class EditNodeData implements Initializable {
+    @FXML
+    private TextField testInput;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // get data for specific ID and pass to view
+        // nothing here we write a custom method yo
+    }
+
+    public void initController(Node data) {
+        testInput.setText(data.getID());
     }
 }
--- src/main/java/controller/NodeDataRead.java
+++ src/main/java/controller/NodeDataRead.java
@@ -3,13 +3,19 @@ package controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javafx.util.Callback;
 import model.DataTable;
 import model.Node;
 
@@ -19,27 +25,51 @@ import java.util.LinkedList;
 import java.util.ResourceBundle;
 
 public class NodeDataRead implements Initializable {
+    // get table
     @FXML
     private TableView<Node> table;
+    // get columns
     @FXML
     private TableColumn nodeID;
     @FXML
     private TableColumn xcoord;
     @FXML
     private TableColumn ycoord;
+    // data to put in the table, consists of model.Node class members
     private ObservableList<Node> data;
 
-    @FXML
-    public void editButtonClicked(ActionEvent e) {
-        try {
-            Parent newSceneRoot = FXMLLoader.load(getClass().getResource("/views/editThing.fxml"));
-            this.table.getScene().setRoot(newSceneRoot);
-        } catch (IOException err) {
-            err.printStackTrace();
+    private Node dataCursor;
+
+    /**
+     * Method to run when an edit button is clicked on the table.
+     * Load new fxml page with data for that Node.
+     *
+     * @param e
+     */
+    EventHandler<ActionEvent> editButtonClicked = new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent e) {
+            dataCursor = new Node("TEST", 12, 52);
+            Stage s = new Stage();
+            FXMLLoader newRoot = null;
+            try {
+                newRoot = FXMLLoader.load(getClass().getResource("/views/edit.fxml"));
+                s.setScene(new Scene(newRoot.load()));
+                newRoot.<EditNodeData>getController().initController(dataCursor);
+                s.show();
+//                table.getScene().setRoot(newRoot.load());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
         }
-    }
+    };
 
-
+    /**
+     * Starting routing for this jfx view.
+     *
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         nodeID.setCellValueFactory(new PropertyValueFactory("ID"));
@@ -47,9 +77,32 @@ public class NodeDataRead implements Initializable {
         ycoord.setCellValueFactory(new PropertyValueFactory("y"));
         DataTable dt = new DataTable();
         this.data = dt.getAllData();
-//        for (Node n : allData) {
-            // add row to table
-//        }
+        // create edit buttons for each row in the table and append
+        TableColumn<Node, Void> colBtn = new TableColumn("Edit");
+        Callback<TableColumn<Node, Void>, TableCell<Node, Void>> cellFactory = new Callback<TableColumn<Node, Void>, TableCell<Node, Void>>() {
+            @Override
+            public TableCell<Node, Void> call(final TableColumn<Node, Void> param) {
+                final TableCell<Node, Void> cell = new TableCell<Node, Void>() {
+                    private final Button btn = new Button("Edit");
+                    {
+                        btn.setOnAction(editButtonClicked);
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+        colBtn.setCellFactory(cellFactory);
+        table.getColumns().add(colBtn);
         table.setItems(this.data);
         table.refresh();
     }
