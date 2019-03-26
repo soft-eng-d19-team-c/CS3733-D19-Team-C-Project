ee58cc28a79698bb4116f8ff5c2eb73a3c680e62
rmlamarche <rmlamarche@wpi.edu>
2019-03-26 19:23:56 -0400

    smaller button sry

--- src/main/java/controller/DownloadScreen.java
+++ src/main/java/controller/DownloadScreen.java
@@ -19,9 +19,7 @@ public class DownloadScreen implements Initializable {
     private DataTable dataTable;
 
     public void setDataTable(DataTable dataTable) {
-        System.out.println(dataTable);
         this.dataTable = dataTable;
-        System.out.println(this.dataTable);
     }
 
     @Override
@@ -30,20 +28,11 @@ public class DownloadScreen implements Initializable {
     }
 
     public void downloadButtonClick(ActionEvent actionEvent) {
-        /*
-            TODO
-            1. we will need to create a datatable here and get all data to download
-            2. trigger download method and return to table page
-         */
-
-        System.out.println(this.dataTable);
         this.dataTable.printToCsv();
         try {
-
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/emptyTable.fxml"));
             Parent newRoot = loader.load();
             downloadButton.getScene().setRoot(newRoot);
-
         } catch (IOException e1) {
             e1.printStackTrace();
         }
