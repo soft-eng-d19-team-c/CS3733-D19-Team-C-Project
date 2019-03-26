eab1145efd9822250040cae25a25191ba4d61a1e
larapadir <lpadir@wpi.edu>
2019-03-26 18:53:34 -0400

    download button click

--- src/main/java/controller/DownloadScreen.java
+++ src/main/java/controller/DownloadScreen.java
@@ -1,12 +1,27 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import model.DataTable;
 
+import javax.xml.crypto.Data;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DownloadScreen implements Initializable {
+    @FXML
+    private TextField downloadButton;
+
+    DataTable dataTable = new DataTable();
+
+    public void setDataTable(DataTable dataTable) {
+        this.dataTable = dataTable;
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -19,5 +34,16 @@ public class DownloadScreen implements Initializable {
             1. we will need to create a datatable here and get all data to download
             2. trigger download method and return to table page
          */
+
+        dataTable.printToCsv();
+        try {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/emptyTable.fxml"));
+            Parent newRoot = loader.load();
+            downloadButton.getScene().setRoot(newRoot);
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
     }
 }
