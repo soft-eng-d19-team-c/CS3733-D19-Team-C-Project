f3659e5efa294eeb6fe4090001e7cf22501ef034
wying8349 <48537562+wying8349@users.noreply.github.com>
2019-03-28 15:13:46 -0400

    Junit tests (not working version)

---
+++ src/test/java/model/DataTableTest.java
@@ -0,0 +1,41 @@
+package model;
+
+import base.Database;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataTableTest {
+    Database db;
+
+    @Before
+    public void setUp() throws Exception {
+         db = new Database();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getAllData() {
+    }
+
+    @Test
+    public void getProjectCNodesByFloor() {
+    }
+
+    @Test
+    public void getDataById() {
+    }
+
+    @Test
+    public void printToCsv() {
+    }
+
+    @Test
+    public void getProjectCEdges() {
+    }
+}
---
+++ src/test/java/model/NodeTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NodeTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+ //   @Test
+//    public void toString() {
+ //   }
+
+    @Test
+    public void update() {
+    }
+}
