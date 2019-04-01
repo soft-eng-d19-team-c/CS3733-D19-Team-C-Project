d3e5ac049fd0d0ce63ed84ec69596be03130851c
rmlamarche <rmlamarche@wpi.edu>
2019-03-31 20:00:38 -0400

    pathfinding stuff

--- src/main/java/controller/Pathfinding.java
+++ src/main/java/controller/Pathfinding.java
@@ -1,11 +1,16 @@
 package controller;
 
 import javafx.fxml.Initializable;
+import model.AStar;
+import model.Node;
 
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 
 public class Pathfinding extends Controller implements Initializable {
+    AStar star;
+    LinkedList<Node> astarResult;
     @Override
     public void init(URL location, ResourceBundle resources) {
         initialize(location, resources);
@@ -13,6 +18,12 @@ public class Pathfinding extends Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        /*
+        String startNode = (String) Main.screenController.getData("startNode");
+        String endNode = (String) Main.screenController.getData("endNode");
+        star = new AStar(startNode, endNode);
+        astarResult = star.findPath();
 
+         */
     }
 }
