3af35d4dcebd41ccada4ed60a27941e595ab9136
rmlamarche <33760077+rmlamarche@users.noreply.github.com>
2019-03-31 21:36:43 -0400

    Revert "1 view"

--- src/main/java/controller/Login.java
+++ src/main/java/controller/Login.java
@@ -1,5 +1,8 @@
 package controller;
 
+import base.EnumScreenType;
+import base.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -14,5 +17,10 @@ public class Login extends Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void loginButtonClick(ActionEvent actionEvent) {
+        Main.user.setPermissions("employee");
+        Main.screenController.setScreen(EnumScreenType.DASHBOARD);
     }
 }
