b7e2bf9506b848c46d1782003da7f25342575bc6
Ryan LaMarche <rmlamarche@wpi.edu>
2019-04-06 10:54:12 -0400

    external transportation init

---
+++ src/main/java/controller/ExternalTransportationServiceController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ExternalTransportationServiceController extends Controller implements Initializable {
+
+
+
+    @Override
+    public void init(URL location, ResourceBundle resources) {
+        initialize(location, resources);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO clear all input fields
+    }
+}
