4fc2408069161fd128de67286456ece89815360c
Ryan LaMarche <rmlamarche@wpi.edu>
2019-04-06 18:48:08 -0400

    this took way too long

--- src/main/java/model/Vector2D.java
+++ src/main/java/model/Vector2D.java
@@ -13,41 +13,15 @@ public class Vector2D {
         this.y2 = y2;
     }
 
-//    public double theta(Vector2D dest){
-//        double dotProduct = this.x * dest.getX()  + this.y * dest.getY() + this.z * dest.getZ();
-//        double magnitude = Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z) * Math.sqrt(dest.getX() * dest.getX() + dest.getY() * dest.getY() + dest.getZ() * dest.getZ());
-//        return Math.acos(dotProduct / magnitude);
-//    }
+    public EnumDirectionType getDirection(Vector2D v) {
+        double dot = this.x2 * v.getX2() + this.y2 * v.getY2(); // dot product
+        double det = this.x2 * v.getY2() - this.y2 * v.getX2(); // determinant
 
-    // get direction
-    // signed length of cross product between 2 vectors
-    // P = (x2 - x1)(y3 - y1) - (y2 - y1)(x3 - x1)
-    // 3 => that.2
-    // 2 = 1
-    // 1 = 2
-    // > 0 means go right
-    // < 0 means go left
+        double angle = Math.atan2(det, dot); // angle in radians from [-pi/2, pi/2]
 
-    // true if left
-    public EnumDirectionType getSignedLengthOfCrossProduct2D(Vector2D v) {
-        double result = (this.x2 - this.x1) * (v.getY2() - this.y1) - (this.y2 - this.y1) * (v.getX2() - this.x1);
-        boolean isStraight = false;
-
-
-        double m = (this.y2 - this.y1) / (this.x2 - this.x1);
-        double b = m * (-this.x1) + this.y1;
-
-
-
-
-
-//        double b = this.y2 / (m * (this.x2 - this.x1));
-
-
-
-        if (isStraight) {
+        if (Math.abs(angle) <= Math.PI / 12) { // +/- 15 degrees
             return EnumDirectionType.STRAIGHT;
-        } else if (result < 0) {
+        } else if (angle < 0) {
             return EnumDirectionType.RIGHT;
         } else {
             return EnumDirectionType.LEFT;
--- src/test/java/model/Vector2DTest.java
+++ src/test/java/model/Vector2DTest.java
@@ -9,13 +9,13 @@ public class Vector2DTest {
     public void testVectorThing() {
         Vector2D v1 = new Vector2D(0,0,1,0);
         Vector2D v2 = new Vector2D(0,0,0,1);
-        assertEquals(EnumDirectionType.LEFT, v1.getSignedLengthOfCrossProduct2D(v2));
+        assertEquals(EnumDirectionType.LEFT, v1.getDirection(v2));
     }
     @Test
     public void testVectorThingRight() {
         Vector2D v1 = new Vector2D(0,0,1,0);
         Vector2D v2 = new Vector2D(0,0,0,1);
-        assertEquals(EnumDirectionType.RIGHT, v2.getSignedLengthOfCrossProduct2D(v1));
+        assertEquals(EnumDirectionType.RIGHT, v2.getDirection(v1));
     }
 
 
@@ -23,19 +23,19 @@ public class Vector2DTest {
     public void testVectorThing2() {
         Vector2D v1 = new Vector2D(0,0,10,0);
         Vector2D v2 = new Vector2D(0,0,0,10);
-        assertEquals(EnumDirectionType.LEFT, v1.getSignedLengthOfCrossProduct2D(v2));
+        assertEquals(EnumDirectionType.LEFT, v1.getDirection(v2));
     }
     @Test
     public void testVectorThingRight2() {
         Vector2D v1 = new Vector2D(0,0,10,0);
         Vector2D v2 = new Vector2D(0,0,0,10);
-        assertEquals(EnumDirectionType.RIGHT, v2.getSignedLengthOfCrossProduct2D(v1));
+        assertEquals(EnumDirectionType.RIGHT, v2.getDirection(v1));
     }
 
     @Test
     public void testVectorThingStraightish() {
         Vector2D v1 = new Vector2D(0,0,1000,100);
-        Vector2D v2 = new Vector2D(0,0,1000,9);
-        assertEquals(EnumDirectionType.STRAIGHT, v2.getSignedLengthOfCrossProduct2D(v1));
+        Vector2D v2 = new Vector2D(0,0,1000,98);
+        assertEquals(EnumDirectionType.STRAIGHT, v2.getDirection(v1));
     }
 }
