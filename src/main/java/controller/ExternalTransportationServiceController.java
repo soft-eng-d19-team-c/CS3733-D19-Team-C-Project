c6e3b69e7d385aa094435bb25a00f28cd3e5336f
Ryan LaMarche <rmlamarche@wpi.edu>
2019-04-07 10:39:19 -0400

    updates

--- src/main/java/controller/ExternalTransportationServiceController.java
+++ src/main/java/controller/ExternalTransportationServiceController.java
@@ -1,12 +1,20 @@
 package controller;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ExternalTransportationServiceController extends Controller implements Initializable {
-
+    @FXML
+    private AutocompleteSearchBarController acSearchController;
+    @FXML
+    private GridPane formGridPane;
 
 
     @Override
@@ -17,5 +25,9 @@ public class ExternalTransportationServiceController extends Controller implemen
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO clear all input fields
+        Platform.runLater(() -> {
+            double acSearchWidth = formGridPane.getColumnConstraints().get(1).getPercentWidth() / 100 * ((Stage) formGridPane.getScene().getWindow()).getWidth() - 100;
+            acSearchController.acTextInput.setPrefWidth(acSearchWidth);
+        });
     }
 }
