921a2e4327446e5b5a15afc3bd02300911effaa4
kheavey <46541966+kheavey@users.noreply.github.com>
2019-04-07 20:37:56 -0400

    Its broken so much

--- src/main/java/controller/GiftStoreController.java
+++ src/main/java/controller/GiftStoreController.java
@@ -1,6 +1,9 @@
 package controller;
 
+import base.EnumScreenType;
+import base.Main;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -11,5 +14,35 @@ import javafx.scene.control.cell.PropertyValueFactory;
 public class GiftStoreController extends Controller implements Initializable {
     @FXML private JFXTextField recipient;
     @FXML private JFXTextField sender;
-    @FXML private
+    @FXML private AutocompleteSearchBar autocompletetextcontroller;
+
+
+    public void submitButtonClick(ActionEvent actionEvent) {
+
+        Main.screenController.setScreen(EnumScreenType.DASHBOARD);
+    }
+
+    public JFXTextField getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(JFXTextField recipient) {
+        this.recipient = recipient;
+    }
+
+    public JFXTextField getSender() {
+        return sender;
+    }
+
+    public void setSender(JFXTextField sender) {
+        this.sender = sender;
+    }
+
+    public AutocompleteSearchBar getAutocompletetextcontroller() {
+        return autocompletetextcontroller;
+    }
+
+    public void setAutocompletetextcontroller(AutocompleteSearchBar autocompletetextcontroller) {
+        this.autocompletetextcontroller = autocompletetextcontroller;
+    }
 }
