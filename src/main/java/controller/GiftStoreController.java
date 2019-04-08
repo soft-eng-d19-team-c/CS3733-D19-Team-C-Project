d0ec2c6710f1759a73f2b5dd9ce71394c507f6ac
kheavey <46541966+kheavey@users.noreply.github.com>
2019-04-07 20:27:29 -0400

    Started Service Request Stuff, including view model and controller. I really don't know what i'm doing

---
+++ src/main/java/controller/GiftStoreController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.Initializable;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class GiftStoreController extends Controller implements Initializable {
+    @FXML private JFXTextField recipient;
+    @FXML private JFXTextField sender;
+    @FXML private
+}
