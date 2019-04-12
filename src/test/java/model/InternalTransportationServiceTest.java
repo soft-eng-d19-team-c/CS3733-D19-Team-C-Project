514f2b316c2d3bbe08cad1bcc3051e619871126c
wying8349 <48537562+wying8349@users.noreply.github.com>
2019-04-12 16:22:05 -0400

    Internal Test

---
+++ src/test/java/model/InternalTransportationServiceTest.java
@@ -0,0 +1,30 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InternalTransportationServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void resolve() {
+    }
+
+    @Test
+    public void getAllServiceRequests() {
+    }
+}
