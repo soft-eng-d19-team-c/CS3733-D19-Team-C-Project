36402a2e045092d0d1182ee5ae6a7b456c0fedc8
wying8349 <48537562+wying8349@users.noreply.github.com>
2019-04-14 15:58:06 -0400

    Prescription Test Although there's no relationship @RYAN

--- src/main/java/controller/PrescriptionServiceController.java
+++ src/main/java/controller/PrescriptionServiceController.java
@@ -2,6 +2,7 @@ package controller;
 
 import base.EnumScreenType;
 import base.Main;
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,6 +14,7 @@ import java.util.ResourceBundle;
 
 public class PrescriptionServiceController extends Controller implements Initializable {
 
+    public JFXButton submitButton;
     @FXML private JFXTextArea patientID;
     @FXML private JFXTextArea prescriptionDescription;
 //    @FXML private JFXButton viewTableButton;
