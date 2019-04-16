e8a5ae58afc1dd6ab45d37680dc9b232792f6ce1
mcburd <mcburd@wpi.edu>
2019-04-16 19:58:11 -0400

    POST port

--- src/main/java/model/RobotPOST.java
+++ src/main/java/model/RobotPOST.java
@@ -22,7 +22,7 @@ public class RobotPOST{
         BufferedReader in = null;
         String result = "";
         try {
-            URL theUrl = new URL("http://ryan-ml.tech/test");
+            URL theUrl = new URL("http://ryan-ml.tech/test:80");
             // build connection
             URLConnection conn = theUrl.openConnection();
             // set request properties
