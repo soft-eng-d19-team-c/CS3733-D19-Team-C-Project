e470b67672c4258c6c213016503f1579d83f8983
Linda Puzey <47217463+lpuzey@users.noreply.github.com>
2019-04-23 12:50:03 -0400

    button works now yaaaaaaaaaay

--- src/main/java/controller/AboutPageController.java
+++ src/main/java/controller/AboutPageController.java
@@ -2,6 +2,7 @@ package controller;
 
 import base.EnumScreenType;
 import base.Main;
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,8 +11,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AboutPageController extends Controller implements Initializable {
-    @FXML
-    private NavController navController;
+    @FXML private NavController navController;
+    @FXML private JFXButton creditsBtn;
 
     @Override
     public void init(URL location, ResourceBundle resources) {
@@ -21,6 +22,7 @@ public class AboutPageController extends Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         navController.setActiveTab(NavTypes.NONE);
+        creditsBtn.toFront();
     }
 
     public void creditsBtnClick(ActionEvent e){
